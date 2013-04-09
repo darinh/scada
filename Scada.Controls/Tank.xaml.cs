d557f1e3080b197e7bfca83f598a85453c3af552
Darin Hoover <darin@hoover.fm>
2013-04-08 22:15:43 -0700

    Styled Tank and added Level Indicator and Value Label.

--- Scada.Controls/Tank.xaml.cs
+++ Scada.Controls/Tank.xaml.cs
@@ -20,6 +20,45 @@ namespace Scada.Controls
     /// </summary>
     public partial class Tank : UserControl
     {
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the Background Brush for the tank.
+        /// </summary>
+        public Brush TankBackgroundColor
+        {
+            get { return TankDrawing.Fill; }
+            set { TankDrawing.Fill = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the Stroke Brush for the tank.
+        /// </summary>
+        public Brush TankStrokeColor
+        {
+            get { return TankDrawing.Stroke; }
+            set { TankDrawing.Stroke = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the StrokeThickness for the tank
+        /// </summary>
+        public double TankStrokeThickness
+        {
+            get { return TankDrawing.StrokeThickness; }
+            set { TankDrawing.StrokeThickness = value; }
+        }
+
+        /// <summary>
+        /// Indicates whether or not the TankGradient is visible;
+        /// </summary>
+        public bool IsTankGradientVisible
+        {
+            get { return TankGradient.IsVisible; }
+            set { TankGradient.Visibility = value ? Visibility.Visible : Visibility.Collapsed;}
+        }
+        #endregion
+
         public Tank()
         {
             InitializeComponent();
@@ -27,6 +66,27 @@ namespace Scada.Controls
             Width = 250;
             Canvas.SetTop(this, 100);
             Canvas.SetLeft(this, 100);
+
+            // Default Colors
+            TankBackgroundColor = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF797979"));
+            TankStrokeColor = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF1F1F1F"));
+            TankStrokeThickness = 2.0;
+            IsTankGradientVisible = true;
+
+            TankFill.Minimum = 0;
+            TankFill.Maximum = 100;
+            TankFill.Value = 1;
+
+            // Water - #FFE6E6E6
+            // Oil - #FF1F1F1F
         }
+
+        private void UserControl_MouseDoubleClick_1(object sender, MouseButtonEventArgs e)
+        {
+            if (TankFill.Value != 100) { TankFill.Value += TankFill.Maximum / 10; } else { TankFill.Value = 0; }
+            System.Diagnostics.Debug.WriteLine(TankFill.Value);
+
+        }
+
     }
 }
